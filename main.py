0b7a1d61955ed445835dde8787d76ffdf1933040
Eduardo Correa Herrera <correaherreraeduardo@gmail.com>
2024-06-23 19:23:09 -0500

    feat: file and new record
    structure to request data for new record

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import numpy as np
+import pandas as pd
+import os
+import re
+from datetime import datetime
+
 def mainMenu():
     print("Welcome to Finance Manager!!!")
     print("1 - Enter a record")
@@ -13,9 +19,71 @@ def getOptions(prompt):
         return value
     except ValueError:
         return defaultValue
+    
+def validateOption(option):
+    if option == 1:
+        setRecord()
+    else:
+        print("The selected option is incorrect")
+
+def setRecord():
+    fileData = openFile()
+    if fileData["status"]:
+        data = fileData["data"]
+        print(getDataNewRecord())
+    else:
+        print("Data empty")
+
+def getDataNewRecord():
+    dateformat = '%Y-%m-%d'
+    while True:
+        date = input("Transaction date (YYYY-MM-DD): ")
+        try:
+            datetime.strptime(date, dateformat)
+            date = datetime.strptime(date, dateformat)
+            date = date.strftime(dateformat)
+            break
+        except ValueError:
+            print("Invalid date. Please enter the date in the format YYYY-MM-DD.")
+    while True:
+        amount = input("Transaction amount: ")
+        if re.match(r'^-?\d+(\.\d+)?$', amount):
+            break
+        else:
+            print("Invalid amount. Please enter a valid number (integer or decimal)")
+    while True:
+        type = input("Transaction type (Entry - Egress): ")
+        if type == "Entry" or type == "Egress":
+            break
+        else:
+            print("Invalid transaction type. Please enter (Entry or Egress)")
+    method = input("Transaction method: ")
+    category = input("Transaction category: ")
+    description = input("Transaction description: ")
+    print("For bank transfers")
+    origin = input("Bank of origin: ")
+    destination = input("Destination bank: ")
+    return {
+        'date': date,
+        'amount': float(amount),
+        'type': type,
+        'method': method,
+        'category': category,
+        'description': description,
+        'origin': origin,
+        'destination': destination
+    }
+
+def openFile():
+    filePath = 'data/transactions.csv'
+    data = {"status": False, "data": []}
+    if os.path.exists(filePath):
+        data = {"status": True, "data": pd.read_csv(filePath)}
+    return data
 
 def run():
-    print(mainMenu())
+    option = mainMenu()
+    validateOption(option)
 
 if __name__ == "__main__":
     run()
